00fa22a8b3c26095a590726b54adcd3196d2b38a
sadhi.cheatmaster@hotmail.com <sadhi.cheatmaster@hotmail.com@8c4b11e0-ecaa-d327-52d1-4d7b7d824953>
2012-05-16 11:06:03 +0000

    added textures for the arena

--- trunk/DualArena/OpenGLDualArena/Arena.h
+++ trunk/DualArena/OpenGLDualArena/Arena.h
@@ -1,18 +1,5 @@
-#include <iostream>
-#include <GLTools.h>            // OpenGL toolkit
-#include <GLShaderManager.h>    // Shader Manager Class
-#include <cmath>
-#include <GLGeometryTransform.h>
-#include <GLMatrixStack.h>
-#include <stdio.h>
 #include <FreeImage.h>
 
-#ifdef __APPLE__
-#include <glut/glut.h>          // OS X version of GLUT
-#else
-#define FREEGLUT_STATIC
-#include <GL/glut.h>            // Windows FreeGlut equivalent
-#endif
 
 void Display(void);
 void Reshape(GLint width, GLint height);
