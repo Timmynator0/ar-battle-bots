1ebfa643e01878ee58b1cc976ffacb456e13993c
sadhi.cheatmaster@hotmail.com <sadhi.cheatmaster@hotmail.com@8c4b11e0-ecaa-d327-52d1-4d7b7d824953>
2012-05-16 09:04:06 +0000

    the beginning of the arena

---
+++ DualArena/OpenGLDualArena/Ramp.cpp
@@ -0,0 +1,58 @@
+#include "Ramp.h"
+
+	void Ramp(void)
+	{
+		glBegin(GL_QUADS);
+		//begin trapeze
+		//zijkant 1
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,0.0f,-12.5f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,5.0f,-4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,5.0f,4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,0.0f,12.5f);
+
+		//zijkant 2
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,0.0f,-12.5f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,5.0f,-4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,5.0f,4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,0.0f,12.5f);
+
+		//zuid ramp
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,0.0f,-12.5f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,5.0f,-4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,5.0f,-4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,0.0f,-12.5f);
+
+		//noord ramp
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,0.0f,12.5f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(7.5f,5.0f,4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,5.0f,4.0f);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-7.5f,0.0f,12.5f);
+
+		//bovenkant ramp
+		glColor3f(1.0f,0.0f,1.0f);
+		glVertex3f(7.5f,5.0f,-4.0f);
+		glColor3f(1.0f,1.0f,1.0f);
+		glVertex3f(7.5f,5.0f,4.0f);
+		glColor3f(1.0f,0.0f,1.0f);
+		glVertex3f(-7.5f,5.0f,4.0f);
+		glColor3f(1.0f,0.0f,1.0f);
+		glVertex3f(-7.5f,5.0f,-4.0f);
+
+		glEnd();
+	}
