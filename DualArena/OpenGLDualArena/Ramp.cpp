f6f4923d0a937c50ae6af3f5786d4bab66c94b75
jcmklund@gmail.com <jcmklund@gmail.com@8c4b11e0-ecaa-d327-52d1-4d7b7d824953>
2012-05-25 12:44:17 +0000

    updated the textures

--- DualArena/OpenGLDualArena/Ramp.cpp
+++ DualArena/OpenGLDualArena/Ramp.cpp
@@ -5,34 +5,34 @@
 		glBegin(GL_QUADS);
 		//begin trapeze
 		//zijkant 1
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,0.0f,-12.5f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,5.0f,-4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,5.0f,4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,0.0f,12.5f);
+		glTexCoord2f(1,1);		glVertex3f(-7.5f,0.0f,-12.5f);
+		glTexCoord2f(1,0);		glVertex3f(-7.5f,5.0f,-4.0f);
+		glTexCoord2f(0,0);		glVertex3f(-7.5f,5.0f,4.0f);
+		glTexCoord2f(0,1);		glVertex3f(-7.5f,0.0f,12.5f);
 
 		//zijkant 2
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,0.0f,-12.5f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,5.0f,-4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,5.0f,4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,0.0f,12.5f);
+		glTexCoord2f(1,1);		glVertex3f(7.5f,0.0f,-12.5f);
+		glTexCoord2f(1,0);		glVertex3f(7.5f,5.0f,-4.0f);
+		glTexCoord2f(0,0);		glVertex3f(7.5f,5.0f,4.0f);
+		glTexCoord2f(0,1);		glVertex3f(7.5f,0.0f,12.5f);
 
 		//zuid ramp
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,0.0f,-12.5f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,5.0f,-4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,5.0f,-4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,0.0f,-12.5f);
+		glTexCoord2f(1,1);		glVertex3f(7.5f,0.0f,-12.5f);
+		glTexCoord2f(1,0);		glVertex3f(7.5f,5.0f,-4.0f);
+		glTexCoord2f(0,0);		glVertex3f(-7.5f,5.0f,-4.0f);
+		glTexCoord2f(0,1);		glVertex3f(-7.5f,0.0f,-12.5f);
 
 		//noord ramp
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,0.0f,12.5f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(7.5f,5.0f,4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,5.0f,4.0f);
-		glColor3f(1.0f,1.0f,0.0f);		glVertex3f(-7.5f,0.0f,12.5f);
+		glTexCoord2f(1,0.2);		glVertex3f(7.5f,0.0f,12.5f);
+		glTexCoord2f(1,0);		glVertex3f(7.5f,5.0f,4.0f);
+		glTexCoord2f(0,0);		glVertex3f(-7.5f,5.0f,4.0f);
+		glTexCoord2f(0,1);		glVertex3f(-7.5f,0.0f,12.5f);
 
 		//bovenkant ramp
-		glColor3f(1.0f,0.0f,1.0f);		glVertex3f(7.5f,5.0f,-4.0f);
-		glColor3f(1.0f,0.0f,1.0f);		glVertex3f(7.5f,5.0f,4.0f);
-		glColor3f(1.0f,0.0f,1.0f);		glVertex3f(-7.5f,5.0f,4.0f);
-		glColor3f(1.0f,0.0f,1.0f);		glVertex3f(-7.5f,5.0f,-4.0f);
+		glTexCoord2f(1,1);		glVertex3f(7.5f,5.0f,-4.0f);
+		glTexCoord2f(1,0);		glVertex3f(7.5f,5.0f,4.0f);
+		glTexCoord2f(0,0);		glVertex3f(-7.5f,5.0f,4.0f);
+		glTexCoord2f(0,1);		glVertex3f(-7.5f,5.0f,-4.0f);
 
 		glEnd();
 	}
