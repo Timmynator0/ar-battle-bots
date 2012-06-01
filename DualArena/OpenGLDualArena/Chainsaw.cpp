5ea383b44c754cfbeb42276c3354820286f07a95
sadhi.cheatmaster@hotmail.com <sadhi.cheatmaster@hotmail.com@8c4b11e0-ecaa-d327-52d1-4d7b7d824953>
2012-06-01 09:59:23 +0000

    chainsaw has textures

--- DualArena/OpenGLDualArena/Chainsaw.cpp
+++ DualArena/OpenGLDualArena/Chainsaw.cpp
@@ -7,7 +7,7 @@ void Chainsaw(float x, float y, float z)
 		glTranslatef(x,y,z);
 		glPushMatrix();
 			glRotated(90,0,1,0);
-			createCircle(5, 5,360,1,false,0,0,0);
+			createCircle(5, 5,360,1,true,0.625,0.625,0.125);
 		glPopMatrix();
 	glPopMatrix();
 }
